f5d195ecbbcc8ceeafd87ee5aa2dd00b54c692b4
Petro Hubskyi <petradix@gmail.com>
2019-06-06 11:38:25 +0300

    delete some shit

--- src/edu/Hillel/JavaForQA/Homework/Lesson_4/Task_1_1/Task_1_1.java
+++ src/edu/Hillel/JavaForQA/Homework/Lesson_4/Task_1_1/Task_1_1.java
@@ -19,37 +19,13 @@ public class Task_1_1 {
         return todayIs;
     }
 
-       public static void tellItLikeItIs(String text) {
 
-        if (day.toString() == todayIs()) {
-
-            switch (day) {
-                case MONDAY:
-                    System.out.println("Mondays are bad.");
-                    break;
-
-                case FRIDAY:
-                    System.out.println("Fridays are better.");
-                    break;
-
-                case SATURDAY:
-                case SUNDAY:
-                    System.out.println("Weekends are best.");
-                    break;
-
-                default:
-                    System.out.println("Midweek days are so-so.");
-                    break;
-            }
-        }
-
-    }
 
 
 
     public static void main(String[] args) {
 
-        tellItLikeItIs(todayIs());
+
 
     }
 }
